e4a8e8ae0405dcfaaf29e859a8132683811c4e62
Dhana0607 <dhanalakshmi.r.2019.cse@rajalakshmi.edu.in>
2025-12-17 12:27:14 +0530

    Built homepage layout:Js interactions

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+const header = document.querySelector('.main_bar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 120) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelector(link.getAttribute('href'))
+        ?.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
 const tabs = document.querySelectorAll('.bestseller_tabs .tab');
 
 tabs.forEach(tab => {
@@ -6,3 +25,30 @@ tabs.forEach(tab => {
     tab.classList.add('active');
   });
 });
+
+document.querySelectorAll('.color_swatches').forEach(group => {
+    const swatches = group.querySelectorAll('.swatch');
+  
+    swatches.forEach(swatch => {
+      swatch.addEventListener('click', () => {
+        swatches.forEach(s => s.classList.remove('active'));
+        swatch.classList.add('active');
+      });
+    });
+});
+  
+const giftingCards = document.querySelectorAll('.gifting_card');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+giftingCards.forEach(card => observer.observe(card));
+
+  
